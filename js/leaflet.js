2437211d2533131f05d10ffa625c50b0f964564a
GillesDBie <s146372@ap.be>
2024-12-12 15:18:39 +0100

    nieuwe pagine 'about', carousel toegevoegd aan project detail, paginas aangepast

--- js/leaflet.js
+++ js/leaflet.js
@@ -1,16 +1,9 @@
-var map = L.map("map").setView([51.23041, 4.4155], 16);
+var map = L.map("map").setView([51.29078, 4.49169], 10);
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-var marker = L.marker([51.23019, 4.416147]).addTo(map);
-
-marker.bindPopup("Campus Ellerman");
-var bounds = [
-  [51.23041, 4.4155],
-  [51.22991, 4.41675],
-];
-L.rectangle(bounds, { color: "#ff7800", weight: 1 }).addTo(map);
+var marker = L.marker([51.29078, 4.49169]).addTo(map);
 
